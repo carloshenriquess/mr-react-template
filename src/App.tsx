21499479cc9b3695355e15054383345e30a71424
Carlos Henrique <carlos.henrique0307200@gmail.com>
2020-07-16 01:38:24 -0300

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import kaori from './assets/kaori.gif';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="l-container">
+      <h1>Best anime</h1>
+      <p className="c-subtitle">Shigatsu wa Kimi no Uso</p>
+      <img src={kaori} alt="Kaori" />
     </div>
   );
 }
